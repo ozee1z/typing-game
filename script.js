53e2dd42af601279b6fc059ccb5a3761c9ff00b7
ozee1z <aanyehschlink@yahoo.com>
2024-11-14 15:09:45 +0000

    Arranged the comments in the code so it looks good. Also removed the function that shows the highScoreButton if highScore length === 10, and used a condition instead in the gitHighScore function to show the highScoreButton if highScore length === 10.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Our quotes
+//Our quotes
 const quotes = [
     'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
     'There is nothing more deceptive than an obvious fact.',
@@ -9,18 +9,18 @@ const quotes = [
     'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
 ];
 
-// Store the list of words and the index of the word the player is currently typing
+//Store the list of words and the index of the word the player is currently typing
 let words = [];
 let wordIndex = 0;
 
-// The starting time and timer status
+//The starting time and timer status
 let startTime;
-let timerStarted = false; // Tracks if the timer has started
+let timerStarted = false; //Tracks if the timer has started
 
 //Speed in wpm
 let speed;
 
-// Page elements
+//Page elements
 const quoteElement = document.getElementById("quote");
 const messageElement = document.getElementById("message");
 const typedValueElement = document.getElementById("typed-value");
@@ -33,65 +33,66 @@ const clearHighScore = document.getElementById("clearScore");
 //Fucntion to start the typing game
 
 startButton.addEventListener("click", () => {
-    // Reset the word index for tracking
+    //Reset the word index for tracking
     wordIndex = 0;
 
-    // Reset timerStarted
+    //Reset timerStarted
     timerStarted = false;
 
-    // Get a quote
+    //Get a quote
     const quoteIndex = Math.floor(Math.random() * quotes.length);
     const quote = quotes[quoteIndex];
 
-    // Put the quote into an array of words
+    //Put the quote into an array of words
     words = quote.split(" ");
   
-    // UI updates
-    // Create an array of span elements so we can set a class
+    //UI updates
+    //Create an array of span elements so we can set a class
     const spanWords = words.map(function(word) { return `<span>${word}</span>`});
 
-    // Convert into string and set as innerHTML on quote display
+    //Convert into string and set as innerHTML on quote display
     quoteElement.innerHTML = spanWords.join(" ");
 
-    // Highlight the first word
+    //Highlight the first word
     quoteElement.children[wordIndex].className = "highlight";
 
-    // Clear any prior messages
+    //Clear any prior messages
     messageElement.innerText = "";
   
-    // Setup the textbox
+    //Setup the textbox
     typedValueElement.disabled = false;
-    // Clear the textbox
+
+    //Clear the textbox
     typedValueElement.value = "";
 
-    // set focus
+    //Set focus
     typedValueElement.focus();
 
-    // Hide high score table
+    //Hide high score table
     highScoreTable.style.display = "none";
   });
 
-// Function when the player starts typing
+//Function when the player starts typing
 
 typedValueElement.addEventListener("input", () => {
-    // Get the current word
+    //Get the current word
     const currentWord = words[wordIndex];
 
-    // Get the current value
+    //Get the current value
     const typedValue = typedValueElement.value;
 
-    // Start the timer if it hasn't started yet
+    //Start the timer if it hasn't started yet
     if (!timerStarted) {
-      startTime = new Date().getTime(); // Time at which you first started typing
-      timerStarted = true; // Set timerStarted to true
+      startTime = new Date().getTime(); //Time at which you first started typing
+      timerStarted = true; //Set timerStarted to true
   }
   
-    // If end of sentence, display success.
+    //If end of sentence, display success.
     if (typedValue === currentWord && wordIndex === words.length - 1) {
-      // Calculate time in seconds
+      //Calculate the time elapsed in seconds
       const elapsedTime = (new Date().getTime() - startTime) / 1000;
 
-      // Calculate the speed in word per minute
+      //Calculate the speed in word per minute
       speed = Math.floor((words.length / elapsedTime) * 60);
       const message = `<p>CONGRATULATIONS!</p> <p>You finished in ${elapsedTime} seconds. Your typing speed is ${speed} WPM.</p>`;
       messageElement.innerHTML = message;
@@ -101,17 +102,16 @@ typedValueElement.addEventListener("input", () => {
       modalElement.style.display = "block";
       saveScore(speed);
       getHighScore();
-      highScoreLength();
     } 
     else if (typedValue.endsWith(" ") && typedValue.trim() === currentWord) {
-      // End of word
-      // Clear the typedValueElement for the new word
+      //End of word
+      //Clear the typedValueElement for the new word
       typedValueElement.value = "";
 
-      // Highlight previous word light green to indicate that it's correct
+      //Highlight previous word green to indicate that it's correct
       quoteElement.children[wordIndex].className = "highlight-green";
 
-      // Move to the next word
+      //Move to the next word
       wordIndex++;
 
       if (wordIndex < quoteElement.children.length) {
@@ -119,11 +119,11 @@ typedValueElement.addEventListener("input", () => {
       }
     } 
     else if (currentWord.startsWith(typedValue)) {
-      // Currently correct, don't apply class name to the input element
+      //Currently correct, don't apply class name to the input element
       typedValueElement.className = "";
     } 
     else {
-      // Make the background color lightcoral to indicate wrong word
+      //Make the background color lightcoral to indicate wrong word
       typedValueElement.className = "error";
     }
   });
@@ -131,7 +131,7 @@ typedValueElement.addEventListener("input", () => {
   //Close the modal when you click outside the modal content
   modalElement.addEventListener("click", (event) => {
   if (event.target === modalElement) {
-      modalElement.style.display = "none"; // Hide the modal
+      modalElement.style.display = "none"; //Hide the modal
     } 
   });
 
@@ -144,12 +144,12 @@ typedValueElement.addEventListener("input", () => {
 function saveScore(wpm) {
   let score = JSON.parse(localStorage.getItem("highScore")) || [];
 
-  // Save the score along with the current timestamp
+  //Save the score along with the current timestamp
   let scoreWithTimestamp = {score: wpm, timestamp: new Date().toLocaleString()};
 
   score.push(scoreWithTimestamp);
-  score.sort((a, b) => b.score - a.score); // Sort by score in descending order
-  score = score.slice(0, 10); // Keep only the top 10 scores
+  score.sort((a, b) => b.score - a.score); //Sort by score in descending order
+  score = score.slice(0, 10); //Keep only the top 10 scores
 
   localStorage.setItem("highScore", JSON.stringify(score));
 }
@@ -159,10 +159,10 @@ function getHighScore() {
   let highScore = JSON.parse(localStorage.getItem("highScore")) || [];
   const highScoresBody = document.getElementById("highScoresBody");
 
-  // Render high scores in the table
+  //Render high scores in the table
   highScoresBody.innerHTML = highScore.map(function (entry, index) {
-    // Parse the timestamp
-    let time = new Date(entry.timestamp); // Use the stored timestamp for each score
+    //Parse the timestamp
+    let time = new Date(entry.timestamp); //Use the stored timestamp for each score
     let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = time.getDay();
     let today = daysOfWeek[day];
@@ -183,12 +183,9 @@ function getHighScore() {
   }).join("");
 
   highScoreTable.style.display = "block";
-};
 
-//show clear highScoreButton if high score length === 10
-function highScoreLength() {
-  let getHighScoreLength = JSON.parse(localStorage.getItem("highScore"));
-  if(getHighScoreLength.length === 10){
+  //Show clear highScoreButton if high score length === 10
+  if(highScore.length === 10) {
     clearHighScore.style.display = "inline";
   }
 };
